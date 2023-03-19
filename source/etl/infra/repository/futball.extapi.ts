43a0fadc4a90a789c4239fe96f981cb0810e190d
leandro <leandrorh21@gmail.com>
2023-03-19 14:14:21 -0500

    [add try catch to api external call]

--- source/etl/infra/repository/futball.extapi.ts
+++ source/etl/infra/repository/futball.extapi.ts
@@ -1,32 +1,41 @@
 import axios from "axios";
+import { GraphQLError } from "graphql";
 import { CompetitionValue } from "../../domain/competition.value";
 import { EtlRepository } from "../../domain/etlRepository";
 import { TeamValue } from "../../domain/team.value";
 
 export class ExternalEtlRepository implements EtlRepository {
   async getCompetitionInfoApi(leagueCode: String): Promise<any> {
-    const competitionFullData = await axios.get(
-      `https://api.football-data.org/v4/competitions/${leagueCode}`,
-      {
-        headers: {
-          "X-Auth-Token": "632dcd4c074a4389bb5b19284dc324fe",
-        },
-      }
-    );
+    try {
+      const competitionFullData = await axios.get(
+        `https://api.football-data.org/v4/competitions/${leagueCode}`,
+        {
+          headers: {
+            "X-Auth-Token": "632dcd4c074a4389bb5b19284dc324fe",
+          },
+        }
+      );
 
-    return competitionFullData.data;
+      return competitionFullData.data;
+    } catch (error) {
+      throw new GraphQLError(error.response.data.message);
+    }
   }
 
   async getTeamPlayersInfoApi(leagueCode: String): Promise<any> {
-    const teamPlayersInfoData = await axios.get(
-      `https://api.football-data.org/v4/competitions/${leagueCode}/teams`,
-      {
-        headers: {
-          "X-Auth-Token": "632dcd4c074a4389bb5b19284dc324fe",
-        },
-      }
-    );
+    try {
+      const teamPlayersInfoData = await axios.get(
+        `https://api.football-data.org/v4/competitions/${leagueCode}/teams`,
+        {
+          headers: {
+            "X-Auth-Token": "632dcd4c074a4389bb5b19284dc324fe",
+          },
+        }
+      );
 
-    return teamPlayersInfoData.data;
+      return teamPlayersInfoData.data;
+    } catch (error) {
+      throw new GraphQLError(error.response.data.message);
+    }
   }
 }
